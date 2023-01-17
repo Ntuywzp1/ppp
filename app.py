61dec61364a5ef88a22cc1e44521735b37ffc713
Ntuywzp1 <Ntuywzp1@gmail.com>
2023-01-16 23:08:39 -0300

    Primeiro commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/ola')
+def ola_mundo():
+    return "Olá,mundo"
+
+
+@app.route('/ola/<nome>')
+def ola_mundo1(nome="mundo"):
+    return "Olá, " + nome
+
+
+if __name__ == '__main__':
+    app.run()
